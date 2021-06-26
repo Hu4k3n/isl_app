af57a2cd9acaca44cc6dc24f492640779e7fe2d6
hu4k3n <arjun.syam23@gmail.com>
2021-06-26 12:32:31 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Stack(
+          children: [
+            Center(child: Text("camera")),
+            Card(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                    height: 100,
+                    width: double.infinity,
+                    color: Colors.yellow,
+                    child: Center(
+                      child: Text(
+                        "text thingy",
+                        style: TextStyle(backgroundColor: Colors.orange),
+                      ),
+                    )),
+              ),
+            ),
+          ],
+        ));
+  }
+}
